[psb120] (fix) Fixed normalisation so that the labels are unchanged

part1_nn_lib.py
@@ -123,7 +123,6 @@ class SigmoidLayer(Layer):
         #                       ** START OF YOUR CODE **
         #######################################################################
         self._cache_current = x
-        print("x", x)
         return self.sigmoid(x)
 
         #######################################################################
@@ -678,7 +677,10 @@ class Preprocessor(object):
         #######################################################################
         #                       ** START OF YOUR CODE **
         #######################################################################
-        return self.a + (data - self.x_min) * (self.b - self.a) / (self.x_max - self.x_min)
+        features = data[:, :-1]
+        labels = data[:, -1:]
+        normalised_features = self.a + (features - self.x_min) * (self.b - self.a) / (self.x_max - self.x_min)
+        return np.concatenate((normalised_features, labels), axis=1)
 
         #######################################################################
         #                       ** END OF YOUR CODE **
@@ -697,12 +699,16 @@ class Preprocessor(object):
         #######################################################################
         #                       ** START OF YOUR CODE **
         #######################################################################
-        dirty_res = self.x_min + (data - self.a) * (self.x_max - self.x_min) / (self.b - self.a)
+        features = data[:, :-1]
+        labels = data[:, -1:]
+
+        initial_features = self.x_min + (features - self.a) * (self.x_max - self.x_min) / (self.b - self.a)
 
         def round_num(x):
             return round(x, 2)
 
-        return np.vectorize(round_num)(dirty_res)
+        rounded_features = np.vectorize(round_num)(initial_features)
+        return np.concatenate((rounded_features, labels), axis=1)
 
         #######################################################################
         #                       ** END OF YOUR CODE **
@@ -712,7 +718,7 @@ class Preprocessor(object):
 def example_main():
     input_dim = 4
     neurons = [16, 3]
-    activations = ["relu", "identity"]
+    activations = ["sigmoid", "identity"]
     net = MultiLayerNetwork(input_dim, neurons, activations)
 
     dat = np.loadtxt("iris.dat")
@@ -748,6 +754,12 @@ def example_main():
         loss_fun="cross_entropy",
         shuffle_flag=True,
     )
+
+    # Test
+
+    # trainer.shuffle()
+
+    # End test
 
     trainer.train(x_train_pre, y_train)
     print("Train loss = ", trainer.eval_loss(x_train_pre, y_train))