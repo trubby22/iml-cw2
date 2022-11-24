2f78b7cdf805351780659830caa23723fa68a1cf
Sanjeet Chatterjee <sc1320@imperial.ac.uk>
2022-11-24 13:49:12 +0000

    remove .pickle from gitignore

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -220,8 +220,7 @@ def save_regressor(trained_model):
     Utility function to save the trained regressor model in part2_model.pickle.
     """
     # If you alter this, make sure it works in tandem with load_regressor
-    print(__import__("os").path.abspath(__import__("os").getcwd()), __import__("os").listdir())
-    with open('./part2_model.pickle', 'wb') as target:
+    with open('part2_model.pickle', 'wb') as target:
         pickle.dump(trained_model, target)
     print("\nSaved model in part2_model.pickle\n")
 
@@ -231,8 +230,7 @@ def load_regressor():
     Utility function to load the trained regressor model in part2_model.pickle.
     """
     # If you alter this, make sure it works in tandem with save_regressor
-    print(__import__("os").path.abspath(__import__("os").getcwd()), __import__("os").listdir())
-    with open('./part2_model.pickle', 'rb') as target:
+    with open('part2_model.pickle', 'rb') as target:
         trained_model = pickle.load(target)
     print("\nLoaded model in part2_model.pickle\n")
     return trained_model
